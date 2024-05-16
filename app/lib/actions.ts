ae4976c580b04c2418245bf389d1a2fbea075be1
unknown <mohseni.amir.eh@gmail.com>
2024-05-16 07:42:24 -0700

    end of chapter 12, just need delete to be emplemented

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,12 +1,62 @@
 'use server';
 
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id:z.string(),
+  customerId:z.string(),
+  //coerce force it to be "number", here
+  amount:z.coerce.number(),
+  status:z.enum(['pending','paid']),
+  date:z.string()
+})
+
+const CreateInvoice = FormSchema.omit({id:true, date:true})
+
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-      };
+  // console.log('forData', formData)
+    const {customerId,amount,status} = CreateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+      });
+
+      const amountInCents = amount * 100;
+      const date = new Date().toISOString().split('T')[0];
+
+      await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+
       // Test it out:
-      console.log(rawFormData);
-      console.log(typeof rawFormData.amount)
+      console.log(formData);
+      console.log(typeof amount)
+
+      revalidatePath('/dashboard/invoices');
+      redirect('/dashboard/invoices')
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
+import { updateInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({
   invoice,
@@ -17,8 +18,12 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  //how to use id for server actions, passing as argument wont work
+  const updateImvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form>
+    <form action={updateImvoiceWithId}>
+      <input hidden name="id" value={invoice.id} />
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
